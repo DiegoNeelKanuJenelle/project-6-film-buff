155a3e1e6e14d35a638eeb2aeee9238b5ac3ccea
Jenelle <lawjenelle@gmail.com>
2019-09-17 10:11:05 -0400

    added router transitions, error handling for english movie search results, changed app title and favicon in index.html, tweaked responsive styles, added animate.css animations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./styles/App.scss";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { spring, AnimatedRoute, AnimatedSwitch } from "react-router-transition";
 import Header from "./Header";
 import EnglishSearchComponent from "./EnglishSearchComponent";
 import ForeignRelatedComponent from "./ForeignRelatedComponent";
@@ -8,10 +9,45 @@ import PublicFeed from "./PublicFeed";
 
 class App extends Component {
   render() {
+    // we need to map the `scale` prop we define below
+    // to the transform style property
+    function mapStyles(styles) {
+      return {
+        opacity: styles.opacity,
+        transform: `scale(${styles.scale})`
+      };
+    }
+
+    // wrap the `spring` helper to use a bouncy config
+    function bounce(val) {
+      return spring(val, {
+        stiffness: 150,
+        damping: 22
+      });
+    }
+    // child matches will...
+    const bounceTransition = {
+      // start in a transparent, upscaled state
+      atEnter: {
+        opacity: 0,
+        scale: 1.2
+      },
+      // leave in a transparent, downscaled state
+      atLeave: {
+        opacity: bounce(0),
+        scale: bounce(0.8)
+      },
+      // and rest at an opaque, normally-scaled state
+      atActive: {
+        opacity: bounce(1),
+        scale: bounce(1)
+      }
+    };
+
     return (
       <Router>
         <div className="App">
-          <nav>
+          <nav className="animated fadeInDown">
             <div className="wrapper">
               <Link to="/">
                 <h3>Film Buff</h3>
@@ -21,12 +57,18 @@ class App extends Component {
               </Link>
             </div>
           </nav>
-          <Switch>
+          <AnimatedSwitch
+            atEnter={bounceTransition.atEnter}
+            atLeave={bounceTransition.atLeave}
+            atActive={bounceTransition.atActive}
+            mapStyles={mapStyles}
+            className="route-wrapper"
+          >
             <Route exact path="/" component={Header} />
             <Route exact path="/English" component={EnglishSearchComponent} />
             <Route exact path="/Foreign" component={ForeignRelatedComponent} />
             <Route exact path="/Feed" component={PublicFeed} />
-          </Switch>
+          </AnimatedSwitch>
         </div>
       </Router>
     );
